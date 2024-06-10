4bbc2891787e5f3bb495db0825239bb9e5eb8182
achuela <asholveram124@gmail.com>
2024-06-10 08:49:47 -0600

    commit

---
+++ dinoplayer/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const SPEED = 500.0
+const JUMP_VELOCITY = -1000.0
+const GRAVITY = 2500
+
+
+func _physics_process(delta):
+	if GHUD.is_playing == true:
+		if is_on_floor():
+			if Input.is_action_pressed("ui_down"):
+				$AnimatedSprite2D.play("dash")
+				$CollisionShape2D.disabled = false
+			else:
+				$CollisionShape2D.disabled = true
+				$AnimatedSprite2D.play("run")
+		else:
+			$AnimatedSprite2D.play("jump")
+			velocity.y += (GRAVITY * delta)
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			$AudioStreamPlayer2D.play()
+			velocity.y = JUMP_VELOCITY
+		velocity.x = SPEED + GHUD.score
+		$RunCollisionShape2D.disabled = !$CollisionShape2D.disabled
+		move_and_slide()
+	else:
+		$AnimatedSprite2D.stop()
---
+++ main.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+@export var mob_scene: PackedScene
+
+var screen_size : Vector2i
+var PlayerInitialPosition
+
+
+func _ready():
+	screen_size = get_window().size
+	PlayerInitialPosition = $Player.position
+	GHUD.start_game_btn.connect(new_game)
+
+func new_game():
+	print('el jeshua')
+	$Player.position = PlayerInitialPosition
+	$Floor.position.x = 0
+	$MOBTimer.start()
+
+func game_over():
+	GHUD.mob_counter = 0
+	$MOBTimer.stop()
+	GHUD.update_highscore()
+
+func _process(delta):
+	$Floor.position.x = $Player.position.x - 150
+
+
+func _on_mob_timer_timeout():
+	if GHUD.mob_counter < 2:
+		var mob = mob_scene.instantiate()
+		mob.position.x = $Player.position.x + 1500
+		mob.position.y = 640
+		add_child(mob)
+		mob.hit.connect(game_over)
